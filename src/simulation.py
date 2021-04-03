9fd148603bb4ab68ad1521a667bb9beabec8a62f
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-04-03 18:10:17 +0200

    Add statistic collecting

--- src/simulation.py
+++ src/simulation.py
@@ -1,6 +1,7 @@
 import time
 from src.agent import Agent
 from src.graphic import Graphic
+from src.statistic import Statistic
 from src.world import World
 import random
 
@@ -8,17 +9,20 @@ import random
 class Simulation:
     graphic: Graphic
     world: World
+    statistic: Statistic
 
     def __init__(self):
         self.world = World(10, 10)
         self.graphic = Graphic(self.world, 400, 400)
         self.world.add_agent(0, 0, Agent(self.world))
         self.world.add_agent(2, 3, Agent(self.world))
+        self.statistic = Statistic(self.world)
 
     def run(self):
         while True:
             self.step()
             self.graphic.render()
+            self.statistic.collect_statistics()
             time.sleep(1)
 
     def step(self):
---
+++ src/statistic.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from src.world import World
+
+
+class Statistic:
+    world: World
+    is_collect_statistics: bool = False
+    result_file = None
+    data_batch: list[str] = []
+    epoch: int = 0
+
+    def __init__(self, world):
+        self.result_file = open(datetime.now().strftime("%d/%m/%Y_%H:%M:%S.csv"), "a")
+        self.result_file.write('epoch,all_agents')
+        self.world = world
+
+    def collect_statistics(self):
+        if not self.is_collect_statistics:
+            return
+
+        epoch = self.epoch
+        all_agents = len(self.world.agents)
+        self.data_batch.append(f'{epoch},{all_agents}')
+        self.epoch += 1
+
+        if len(self.data_batch) > 100:
+            self.result_file.write('\n'.join(self.data_batch))
+            self.data_batch = []
+
